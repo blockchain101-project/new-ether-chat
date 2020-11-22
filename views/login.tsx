b72d9c52db0837d2bee49795a6d21fbae70a71a0
Purinut Thedwichienchai <purinatth@gmail.com>
2020-11-23 00:27:35 +0700

    update login page

--- views/login.tsx
+++ views/login.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, useEffect, useState} from 'react'
 import {
   Button,
   Card, Container, Form, Grid, Header, Label
@@ -8,21 +8,32 @@ import styled from 'styled-components';
 import 'semantic-ui-css/semantic.min.css';
 
 const Login = () => {
+  const [isMetamask, setIsMetamask] = useState(false);
   const router = useRouter();
 
-  const onSubmit = () => {  
-    router.push({
-      pathname: '/home'
-    });
-  }
+  useEffect(() => {
+    if(isMetamask){
+      router.push({
+        pathname: '/home'
+      });
+    }
+  }, [])
+
+  useEffect(() => {
+    if(isMetamask){
+      router.push({
+        pathname: '/home'
+      });
+    }
+  }, [isMetamask])
 
   return(
     <Grid style={{margin: 0, position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
       <Grid.Row centered>
         <Card color='blue'>
           <Card.Content>
-            <Card.Header as='h2' textAlign='center'>Welcome to chat</Card.Header>
-            <Form size='large' onSubmit={() => onSubmit()}>
+            <Card.Header as='h2' textAlign='center'>You need to login to Metamask before being able to use this app.</Card.Header>
+            {/* <Form size='large' onSubmit={() => onSubmit()}>
               <Form.Field>
                 <label style={{textAlign: 'left'}}>Public key</label>
                 <input placeholder="public key"></input>
@@ -32,7 +43,7 @@ const Login = () => {
                 <input placeholder="private key"></input>
               </Form.Field>
               <Button color='blue'>Login</Button>
-            </Form>
+            </Form> */}
           </Card.Content>
         </Card>
       </Grid.Row>
